f7687c0d90098e93a893c9a45fdb67cf6ac090f6
smallproject <j.a.villas@live.com.ph>
2024-10-15 11:36:14 +0200

    enclosed in section element

--- src/pages/Login-Auth/Login-Auth.jsx
+++ src/pages/Login-Auth/Login-Auth.jsx
@@ -7,23 +7,25 @@ export const description =
 
 function LoginAuth() {
     return (
-        <div className="container">
-            <div className={"login-container"}>
-                <h2>Login</h2>
-                <h7>Enter your email below to login to your account</h7>
-                <form action="/authenticate" method={"POST"}>
-                    <div className={"input-group"}>
-                        <label htmlFor="username">Username</label>
-                        <input type="text" id={"username"} name={"username"} placeholder={"@books are nice.com"} required={true}/>
-                    </div>
-                    <div className={"input-group"}>
-                        <label htmlFor="passwored">Password</label>
-                        <input type="password" id={"password"} name={"password"} placeholder={"hello world"} required={true}/>
-                    </div>
-                    <button type={"submit"} className={"login-btn"}>Login</button>
-                </form>
+        <section>
+            <div className="container">
+                <div className={"login-container"}>
+                    <h2>Login</h2>
+                    <h6>Enter your email below to login to your account</h6>
+                    <form action="/authenticate" method={"POST"}>
+                        <div className={"input-group"}>
+                            <label htmlFor="username">Username</label>
+                            <input type="text" id={"username"} name={"username"} placeholder={"@books are nice.com"} required={true}/>
+                        </div>
+                        <div className={"input-group"}>
+                            <label htmlFor="passwored">Password</label>
+                            <input type="password" id={"password"} name={"password"} placeholder={"hello world"} required={true}/>
+                        </div>
+                        <button type={"submit"} className={"login-btn"}>Login</button>
+                    </form>
+                </div>
             </div>
-        </div>
+        </section>
     );
 }
 
