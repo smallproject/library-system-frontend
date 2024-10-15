2b9a5004c6353f7994ae70b6a10d8e121cf71fa2
smallproject <j.a.villas@live.com.ph>
2024-10-15 11:37:16 +0200

    enclosed in section element

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 function AboutUs(props) {
     return (
-        <>
+        <section>
             <h1>About Us</h1>
             <p>We are dedicated to providing access to a world of knowledge through books and resources.</p>
-        </>
+        </section>
     );
 }
 
