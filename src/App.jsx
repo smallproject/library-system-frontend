dc1536968b1ee6649b6996767ddd73c75c43a8ae
smallproject <j.a.villas@live.com.ph>
2024-10-15 10:38:52 +0200

    added main routes in App.jsx for: catalog about contact login

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import './App.css'
-import GetEndpointCard from "./components/GetEndpoint/GetEndpointCard.jsx";
 import {Route, Routes} from "react-router-dom";
 import React from "react";
 import AuthorMain from "./models/Author/AuthorMain.jsx";
@@ -20,20 +19,15 @@ function App() {
     <>
         <div className={"App"}>
             <Navbar/>
-            <div className="test">
-                <h1>Home page</h1>
-            </div>
-            <div className={"main"}></div>
-            <div className={"section"}>
-                <h2>SECTION</h2>
+
+            <div className={"main-content"}>
                 <Routes>
-                    <Route path={"/authors/*"} element={<AuthorMain/>}/>
-                    <Route path={"/books"}
-                           element={<GetEndpointCard title={"BOOK"} url={"http://localhost:8080/api/v1/books/1"}/>}/>
-                    <Route path={"/inventories"} element={<GetEndpointCard title={"INVENTORY"}
-                                                                           url={"http://localhost:8080/api/v1/inventory/1"}/>}/>
-                    <Route path={"/userreviews"} element={<GetEndpointCard title={"USER REVIEW"}
-                                                                           url={"http://localhost:8080/api/v1/userreviews/1"}/>}/>
+                    <Route path="/catalog" element={<Catalog />} />
+                    <Route path="/about" element={<AboutUs />} />
+                    <Route path="/contact" element={<Contact />} />
+                    {/*<Route path="/contact" element={<Contact />} />*/}
+                    {/*<Route path="/contact" element={<Contact />} />*/}
+                    <Route path="/login" element={<Login />} />
                 </Routes>
             </div>
 
