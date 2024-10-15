dbee666fdf53fa440cd2a8fc2cf29ec2fdcb674a
smallproject <j.a.villas@live.com.ph>
2024-10-15 10:32:32 +0200

    added the catalog page - temp

---
+++ src/pages/Catalog.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Catalog(props) {
+    return (
+        <>
+            <div>
+                <h2>Book Catalog</h2>
+                <p>Browse through our extensive collection of books across various genres.</p>
+            </div>
+        </>
+    );
+}
+
+export default Catalog;
