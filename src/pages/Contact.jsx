2097381c5a54f249105649882908ce18129af90f
smallproject <j.a.villas@live.com.ph>
2024-10-15 11:35:41 +0200

    enclosed in section element

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 function Contact(props) {
     return (
-        <>
+        <section>
             <div>
                 <h2>Contact Us</h2>
                 <p>Email: info@librarysystemtest.com</p>
                 <p>Phone: +316-5467-4564</p>
             </div>
-        </>
+        </section>
     );
 }
 
