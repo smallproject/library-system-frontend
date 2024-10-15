ef0648594d34424d2a096457a1cc4fbae389c14e
smallproject <j.a.villas@live.com.ph>
2024-10-15 11:35:33 +0200

    enclosed in section element

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 function Catalog(props) {
     return (
-        <>
+        <section>
             <div>
                 <h2>Book Catalog</h2>
                 <p>Browse through our extensive collection of books across various genres.</p>
             </div>
-        </>
+        </section>
     );
 }
 
