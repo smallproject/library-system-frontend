acaf3a3625c39fd2bc113fe3e06ef690ccd1512e
smallproject <j.a.villas@live.com.ph>
2024-10-15 10:31:57 +0200

    added the contact page - temp

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Contact(props) {
+    return (
+        <>
+            <div>
+                <h2>Contact Us</h2>
+                <p>Email: info@librarysystemtest.com</p>
+                <p>Phone: +316-5467-4564</p>
+            </div>
+        </>
+    );
+}
+
+export default Contact;
