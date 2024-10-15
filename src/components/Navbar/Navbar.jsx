199b07afb812c3c14d1906d697d4c16d0db21a81
smallproject <j.a.villas@live.com.ph>
2024-10-15 11:53:02 +0200

    cleancoded

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -12,38 +12,22 @@ const Navbar = () => {
     }
 
     return (
-        <>
-            <nav className={"navbar"}>
-                <div className={"navbar-logo"}>
-                    <h1>Library System Biblio</h1>
-                </div>
-                <div className={`navbar-links ${isOpen ? "open" : ""}`}>
-                {/*<div className={`navbar-links `}>*/}
-                    <Link to={"/"}>Home</Link>
-                    <Link to={"/catalog"}>Catalog</Link>
-                    <Link to={"/about"}>About Us</Link>
-                    <Link to={"/contact"}>Contact</Link>
-                    {/*<a href="#contact">Contact</a>*/}
-                    {/*<a href="#login">Login</a>*/}
-                    <Link to={"/login"}>Login</Link>
-                </div>
+        <nav className={"navbar navigation"}>
+            <div className={"navbar-logo"}>
+                <h1>Library System Biblio</h1>
+            </div>
+            <div className={`navbar-links ${isOpen ? "open" : ""}`}>
+                <Link to={"/"}>Home</Link>
+                <Link to={"/catalog"}>Catalog</Link>
+                <Link to={"/about"}>About Us</Link>
+                <Link to={"/contact"}>Contact</Link>
+                <Link to={"/login"}>Login</Link>
+            </div>
 
-                <div className={"navbar-toggle"} onClick={toggleNavbar}>
-                    {isOpen ? <FaTimes/> : <FaBars/>}
-                    {/*<FaTimes/>*/}
-                </div>
-            </nav>
-
-
-            {/*<div className="navbar">*/}
-            {/*    <ul>*/}
-            {/*        <li><Link to={"/authors"}>AUTHORS</Link></li>*/}
-            {/*        <li><Link to={"/books"}>BOOKS</Link></li>*/}
-            {/*        <li><Link to={"/inventories"}>INVENTORIES</Link></li>*/}
-            {/*        <li><Link to={"/userreviews"}>USER REVIEWS</Link></li>*/}
-            {/*    </ul>*/}
-            {/*</div>*/}
-        </>
+            <div className={"navbar-toggle"} onClick={toggleNavbar}>
+                {isOpen ? <FaTimes/> : <FaBars/>}
+            </div>
+        </nav>
     );
 }
 
