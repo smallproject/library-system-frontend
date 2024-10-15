7d5622c34ed92237248b50702555c106844fb7db
smallproject <j.a.villas@live.com.ph>
2024-10-15 11:34:28 +0200

    added notfound page

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function NotFound(props) {
+    return (
+        <section>
+            <h1>404 - Page Not Found</h1>
+            <p>Sorry, the page you're looking for doesn't exist.</p>
+        </section>
+    );
+}
+
+export default NotFound;
