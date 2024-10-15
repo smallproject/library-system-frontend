f81fefce536b86fb250d6e35d27c935c6fc49a3b
smallproject <j.a.villas@live.com.ph>
2024-10-15 11:56:36 +0200

    added the basic structure of the main body for app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,48 @@
 import './App.css'
 import {Route, Routes} from "react-router-dom";
-import React from "react";
-import AuthorMain from "./models/Author/AuthorMain.jsx";
-import Navbar from "./components/Navbar/Navbar.jsx";
+import Main from "./pages/Main.jsx";
+
 import "./App.css"
 import Login from "./pages/Login-Auth/Login-Auth.jsx";
 import AboutUs from "./pages/AboutUs.jsx";
 import GetEndpointCard from "./components/GetEndpoint/GetEndpointCard.jsx";
 import GetAllEndpointTiles from "./components/GetAllEndpoint/GetAllEndpointTiles.jsx";
-import Navbar from "./components/Navbar/Navbar.jsx";
 import Footer from "./components/Footer/Footer.jsx";
 import Catalog from "./pages/Catalog.jsx";
 import Contact from "./pages/Contact.jsx";
+import NotFound from "./pages/NotFound.jsx";
+import Header from "./components/Header/Header.jsx";
 
 function App() {
 
   return (
     <>
         <div className={"App"}>
-            <Navbar/>
+            <Header/>
 
-            <div className={"main-content"}>
+            <main className={"main-content container"}>
                 <Routes>
+                    <Route path="/" element={<Main />}>
+                        <Route path={"/books"} element={<GetAllEndpointTiles title={"Authors"} url={`http://localhost:8080/api/v1/books/`}/>}>
+                            <Route path={":id"} element={<GetEndpointCard title={"GetBy"} url={`http://localhost:8080/api/v1/books/`}/>}/>
+                        </Route>
+                        <Route path={"/authors"} element={<GetAllEndpointTiles title={"Authors"} url={`http://localhost:8080/api/v1/authors/`}/>}>
+                            <Route path={":id"} element={<GetEndpointCard title={"GetBy"} url={`http://localhost:8080/api/v1/authors/`}/>}/>
+                        </Route>
+                        <Route path={"/inventories"} element={<GetAllEndpointTiles title={"Authors"} url={`http://localhost:8080/api/v1/inventories/`}/>}>
+                            <Route path={":id"} element={<GetEndpointCard title={"GetBy"} url={`http://localhost:8080/api/v1/inventories/`}/>}/>
+                        </Route>
+                        <Route path={"/userreviews"} element={<GetAllEndpointTiles title={"Authors"} url={`http://localhost:8080/api/v1/userreviews/`}/>}>
+                            <Route path={":id"} element={<GetEndpointCard title={"GetBy"} url={`http://localhost:8080/api/v1/userreviews/`}/>}/>
+                        </Route>
+                    </Route>
                     <Route path="/catalog" element={<Catalog />} />
                     <Route path="/about" element={<AboutUs />} />
                     <Route path="/contact" element={<Contact />} />
-                    {/*<Route path="/contact" element={<Contact />} />*/}
-                    {/*<Route path="/contact" element={<Contact />} />*/}
                     <Route path="/login" element={<Login />} />
+                    <Route path="/notfound" element={<NotFound />} />
                 </Routes>
-            </div>
+            </main>
 
             <Footer/>
         </div>
