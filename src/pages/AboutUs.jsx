922de83ae935573a6cd1c16231cb8dd1b282d065
smallproject <j.a.villas@live.com.ph>
2024-10-15 10:32:16 +0200

    added the about us page - temp

---
+++ src/pages/AboutUs.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AboutUs(props) {
+    return (
+        <>
+            <h1>About Us</h1>
+            <p>We are dedicated to providing access to a world of knowledge through books and resources.</p>
+        </>
+    );
+}
+
+export default AboutUs;
