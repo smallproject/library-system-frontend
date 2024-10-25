990f1197d7347b50d60c2ab5c0c7d80da909ae42
smallproject <j.a.villas@live.com.ph>
2024-10-25 13:58:23 +0200

    added language context provider on global level

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,20 +1,25 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import "./Navbar.css";
 import {Link} from "react-router-dom";
 import { FaBars, FaTimes } from 'react-icons/fa';
+import {LanguageContext} from "../../context/LanguageContext/LanguageContext.jsx";
 
 
 const Navbar = () => {
     const [isOpen, setIsOpen] = React.useState(false);
+    const {language, setLanguage} = useContext(LanguageContext);
+    const selectLanguage = (e) => { setLanguage(e.target.value) }
 
     const toggleNavbar = () => {
         setIsOpen(!isOpen);
     }
 
+
+
     return (
         <nav className={"navbar navigation"}>
             <div className={"navbar-logo"}>
-                <h1>Library System Biblio</h1>
+                <h1>Library System Libro</h1>
             </div>
             <div className={`navbar-links ${isOpen ? "open" : ""}`}>
                 <Link to={"/"}>Home</Link>
@@ -24,8 +29,15 @@ const Navbar = () => {
                 <Link to={"/login"}>Login</Link>
             </div>
 
+            <br/>
             <div className={"navbar-toggle"} onClick={toggleNavbar}>
                 {isOpen ? <FaTimes/> : <FaBars/>}
+            </div>
+            <div className={"language-toggle"}>
+                <select value={language} onChange={selectLanguage}>
+                    <option value="nl">NL</option>
+                    <option value="en">EN</option>
+                </select>
             </div>
         </nav>
     );
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import {BrowserRouter as Router} from "react-router-dom";
+import LanguageContextProvider from "./context/LanguageContext/LanguageContext.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
       <Router>
-          <App />
+          <LanguageContextProvider>
+            <App />
+          </LanguageContextProvider>
       </Router>
   </StrictMode>,
 )
