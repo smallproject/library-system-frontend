f4beb317b6133c760dcd604a78fc39c8d9d624c9
smallproject <j.a.villas@live.com.ph>
2024-10-25 12:12:11 +0200

    updated about us content

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import "/src/App.css"
 
-function AboutUs(props) {
+function AboutUs() {
     return (
         <section>
-            <h1>About Us</h1>
-            <p>We are dedicated to providing access to a world of knowledge through books and resources.</p>
+            <article className={"aboutus"}>
+                <h1>About Us</h1>
+                <br/>
+                <p>We are dedicated to providing access to a world of knowledge through books and resources.</p>
+                <br/>
+                <p>
+                    Welcome to Libro! Our mission is to make knowledge and literature accessible to everyone, anywhere, at any time. Founded with a passion for reading and a commitment to lifelong learning, we strive to provide a digital platform that connects people with books, articles, research materials, and multimedia resources across a range of subjects and genres. Whether you&#39;re a student, a professional, or simply a book lover, Libro has something for everyone, with a carefully curated collection to inspire, educate, and entertain.
+                </p>
+                <br/>
+                <br/>
+                <p>
+                    Our team is dedicated to creating an intuitive, user-friendly experience where you can explore, discover, and engage with new ideas. We believe that everyone should have the chance to dive into a world of knowledge without limitations, and our 24/7 platform reflects this. Join us on this journey to expand your horizons and connect with a community of like-minded readers. At Libro, learning never stops.
+                </p>
+            </article>
         </section>
     );
 }
