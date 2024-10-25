68b455a4593a015871a56309bbe518ab30e2a584
smallproject <j.a.villas@live.com.ph>
2024-10-25 13:55:48 +0200

    updated multi-language about us page

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,20 +1,28 @@
 import "/src/App.css"
+import content from "../content/content.json";
+import {useContext} from "react";
+import {LanguageContext} from "../context/LanguageContext/LanguageContext.jsx";
 
 function AboutUs() {
+    const {language} = useContext(LanguageContext);
+    const {title, description, description2, description3} = content[language].about;
+
     return (
         <section>
             <article className={"aboutus"}>
-                <h1>About Us</h1>
-                <br/>
-                <p>We are dedicated to providing access to a world of knowledge through books and resources.</p>
+                <h1>{title}</h1>
                 <br/>
                 <p>
-                    Welcome to Libro! Our mission is to make knowledge and literature accessible to everyone, anywhere, at any time. Founded with a passion for reading and a commitment to lifelong learning, we strive to provide a digital platform that connects people with books, articles, research materials, and multimedia resources across a range of subjects and genres. Whether you&#39;re a student, a professional, or simply a book lover, Libro has something for everyone, with a carefully curated collection to inspire, educate, and entertain.
+                    {description}
+                </p>
+                <br/>
+                <p>
+                    {description2}
                 </p>
                 <br/>
                 <br/>
                 <p>
-                    Our team is dedicated to creating an intuitive, user-friendly experience where you can explore, discover, and engage with new ideas. We believe that everyone should have the chance to dive into a world of knowledge without limitations, and our 24/7 platform reflects this. Join us on this journey to expand your horizons and connect with a community of like-minded readers. At Libro, learning never stops.
+                    {description3}
                 </p>
             </article>
         </section>
