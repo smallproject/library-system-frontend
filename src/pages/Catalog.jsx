917a7c0d07937bc6022bc2f27c1c165978aa8f20
smallproject <j.a.villas@live.com.ph>
2024-10-25 12:13:27 +0200

    updated catalog content

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import "/src/App.css";
 
-function Catalog(props) {
+function Catalog() {
     return (
         <section>
-            <div>
-                <h2>Book Catalog</h2>
+            <article className={"catalog"}>
+                <h1>Book Catalog</h1>
+                <br/>
                 <p>Browse through our extensive collection of books across various genres.</p>
-            </div>
+                <p>
+                    Welcome to Libro, your ultimate destination for accessing an extensive collection of books, journals, articles, and multimedia resources from the comfort of your own space. Our online library system is designed to offer users a seamless, interactive experience that brings the world of knowledge to their fingertips. With advanced search features, personalized reading recommendations, and easy-to-navigate categories, finding the perfect resource has never been simpler. Whether you&#39;re researching academic material, diving into fiction, or exploring new hobbies, Libro offers something for every reader and learner.
+
+                </p>
+                <br/>
+                <br/>
+
+                <p>
+                    Our system not only supports quick and efficient access to materials but also enhances learning through features like reading history, bookmarks, and in-depth annotations, allowing users to create a personalized digital library experience. With resources available 24/7, you can learn, explore, and grow on your own schedule. Join a growing community of book lovers, students, and researchers who rely on Libro for quality content, curated collections, and a unique reading experience designed to inspire lifelong learning.
+                </p>
+            </article>
         </section>
     );
 }
