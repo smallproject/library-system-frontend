b6fb7010108e36d0cacc17c6a6589a04bd41894b
smallproject <j.a.villas@live.com.ph>
2024-10-25 13:55:16 +0200

    added multi-language catalog page

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,21 +1,26 @@
 import "/src/App.css";
+import {useContext} from "react";
+import {LanguageContext} from "../context/LanguageContext/LanguageContext.jsx";
+import content from "../content/content.json"
 
 function Catalog() {
+    const {language} = useContext(LanguageContext);
+    const {title, description, description2, description3} = content[language].catalog;
     return (
         <section>
             <article className={"catalog"}>
-                <h1>Book Catalog</h1>
+                <h1>{title}</h1>
                 <br/>
-                <p>Browse through our extensive collection of books across various genres.</p>
                 <p>
-                    Welcome to Libro, your ultimate destination for accessing an extensive collection of books, journals, articles, and multimedia resources from the comfort of your own space. Our online library system is designed to offer users a seamless, interactive experience that brings the world of knowledge to their fingertips. With advanced search features, personalized reading recommendations, and easy-to-navigate categories, finding the perfect resource has never been simpler. Whether you&#39;re researching academic material, diving into fiction, or exploring new hobbies, Libro offers something for every reader and learner.
-
+                    {description}
+                </p>
+                <p>
+                    {description2}
                 </p>
                 <br/>
                 <br/>
-
                 <p>
-                    Our system not only supports quick and efficient access to materials but also enhances learning through features like reading history, bookmarks, and in-depth annotations, allowing users to create a personalized digital library experience. With resources available 24/7, you can learn, explore, and grow on your own schedule. Join a growing community of book lovers, students, and researchers who rely on Libro for quality content, curated collections, and a unique reading experience designed to inspire lifelong learning.
+                    {description3}
                 </p>
             </article>
         </section>
