74eb9bce245178a33cfa8a16736d8bf8d1614c3b
smallproject <j.a.villas@live.com.ph>
2024-10-29 15:26:46 +0100

    implemented AuthContext in Main

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,18 @@ import App from './App.jsx'
 import './index.css'
 import {BrowserRouter as Router} from "react-router-dom";
 import LanguageContextProvider from "./context/LanguageContext.jsx";
+import ThemeContextProvider from "./context/ThemeContext.jsx";
+import AuthContextProvider from "./context/AuthContext.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
       <Router>
           <LanguageContextProvider>
-            <App />
+              <ThemeContextProvider>
+                  <AuthContextProvider>
+                    <App />
+                  </AuthContextProvider>
+              </ThemeContextProvider>
           </LanguageContextProvider>
       </Router>
   </StrictMode>,
