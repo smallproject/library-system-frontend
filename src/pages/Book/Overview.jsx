d473d3d83af03e8780b2a9404951fb65dbb6ad5c
smallproject <j.a.villas@live.com.ph>
2024-11-01 05:43:57 +0100

    updated book overview replaced author and price column with genre and publication date

--- src/pages/Book/Overview.jsx
+++ src/pages/Book/Overview.jsx
@@ -59,9 +59,8 @@ function Overview() {
                     <tr>
                         <th>ISBN</th>
                         <th>Title</th>
-                        <th>Author</th>
-                        <th>Price</th>
-                        <th>Actions</th>
+                        <th>Genre</th>
+                        <th>Publication Date</th>
                     </tr>
                     </thead>
                     <tbody>
@@ -70,8 +69,8 @@ function Overview() {
                             <tr key={book.id}>
                                 <td>{book.isbn}</td>
                                 <td>{book.title}</td>
-                                <td>{book.author}</td>
-                                <td>{book.price}</td>
+                                <td>{book.genre}</td>
+                                <td>{book.publicationDate}</td>
                                 <td>
                                     <button
                                         onClick={() => navigate(`/api/v1/books/${book.id}`)}
