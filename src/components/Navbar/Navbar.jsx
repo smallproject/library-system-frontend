8ecab78d518a47d931117fe8fc869a96d63e00ca
smallproject <j.a.villas@live.com.ph>
2024-11-14 15:43:37 +0100

    redesign navbar for better user experience

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,6 @@ import React, {useContext} from 'react';
 import "./Navbar.css";
 import {Link, useNavigate} from "react-router-dom";
 import {FaBars, FaTimes} from 'react-icons/fa';
-import {LanguageContext} from "../../context/LanguageContext.jsx";
 import {ThemeContext} from "../../context/ThemeContext.jsx";
 import {AuthContext} from "../../context/AuthContext.jsx";
 
@@ -10,10 +9,6 @@ import {AuthContext} from "../../context/AuthContext.jsx";
 function Navbar() {
     const {isAuth, logout} = useContext(AuthContext);
     const [isOpen, setIsOpen] = React.useState(false);
-    const {language, setLanguage} = useContext(LanguageContext);
-    const selectLanguage = (e) => {
-        setLanguage(e.target.value)
-    }
     const navigate = useNavigate();
 
     const toggleNavbar = () => {
@@ -26,11 +21,43 @@ function Navbar() {
     return (
         <nav className={"container navbar"}>
             <div className={"row-1"}>
+
                 <div>
-                    <Link to={"/api/v1/books"}>Books</Link>
-                    <Link to={"/api/v1/authors"}>Authors</Link>
-                    <Link to={"/api/v1/userreviews"}>User Reviews</Link>
-                    <Link to={"/api/v1/inventories"}>Inventories</Link>
+
+                    {isAuth ? (
+                        <div>
+                            <button
+                                type="button"
+                                onClick={logout}
+                            >
+                                Log out
+                            </button>
+                        </div>
+                    ) : (
+                        <div>
+                            <button
+                                type="button"
+                                onClick={() => navigate('/signin')}
+                            >
+                                Log in
+                            </button>
+                            {/*<button*/}
+                            {/*    type="button"*/}
+                            {/*    onClick={() => navigate('/signup')}*/}
+                            {/*>*/}
+                            {/*    Registreren*/}
+                            {/*</button>*/}
+                        </div>
+                    )};
+
+                </div>
+                <div className={"row-1-content container"}>
+                    {/*<Link to={"/api/v1/books"}>Books</Link>*/}
+                    {/*<Link to={"/api/v1/authors"}>Authors</Link>*/}
+                    {/*<Link to={"/api/v1/userreviews"}>User Reviews</Link>*/}
+                    {/*<Link to={"/api/v1/inventories"}>Inventories</Link>*/}
+                    <input className={"search-bar"} type="text" placeholder={"What are you looking for?"}/>
+                    <button className={"search-button"}>Search</button>
                 </div>
 
                 {/*<div className={"theme-toggle"} style={{*/}
@@ -45,47 +72,13 @@ function Navbar() {
                 {/*    </button>*/}
                 {/*</div>*/}
 
-                {isAuth ? (
-                    <div>
-                        <button
-                            type="button"
-                            onClick={logout}
-                        >
-                            Log out
-                        </button>
-                    </div>
-                ) : (
-                    <div>
-                        <button
-                            type="button"
-                            onClick={() => navigate('/signin')}
-                        >
-                            Log in
-                        </button>
-                        <button
-                            type="button"
-                            onClick={() => navigate('/signup')}
-                        >
-                            Registreren
-                        </button>
-                    </div>
-                )};
-
-
-                <div className={"language-toggle"}>
-                    <select value={language} onChange={selectLanguage}>
-                        <option value="nl">NL</option>
-                        <option value="en">EN</option>
-                    </select>
-                </div>
             </div>
             <div className={"row-2"}>
 
                 <div className={"navbar-logo"}>
-                    <h1>Library System Libro</h1>
+                    <Link to={"/"}><h1>Library System Libro</h1></Link>
                 </div>
                 <div className={`navbar-links ${isOpen ? "open" : ""}`}>
-                    <Link to={"/"}>Home</Link>
                     <Link to={"/catalog"}>Catalog</Link>
                     <Link to={"/about"}>About Us</Link>
                     <Link to={"/contact"}>Contact</Link>
