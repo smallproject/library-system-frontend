17566f15120e447aa8fc1be88233641261ab7970
smallproject <j.a.villas@live.com.ph>
2024-11-14 16:04:15 +0100

    added language context to footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import "./Footer.css";
+import {LanguageContext} from "../../context/LanguageContext.jsx";
 
-function Footer(props) {
+function Footer() {
+    const {language, setLanguage} = useContext(LanguageContext);
+    const selectLanguage = (e) => {
+        setLanguage(e.target.value)
+    }
     return (
         <>
-            <footer className={"footer"}>
+            <footer className={"container footer"}>
+
+
+                <div className={"language-toggle"}>
+                    <select value={language} onChange={selectLanguage}>
+                        <option value="nl">NL</option>
+                        <option value="en">EN</option>
+                    </select>
+                </div>
                 <div className={"footer-content"}>
                     <div className={"footer-section library-info"}>
                         <h4>Library System</h4>
