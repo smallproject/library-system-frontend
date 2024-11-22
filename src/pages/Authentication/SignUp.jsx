d8d73945396ae881d34a942d24eab571a0e4d090
smallproject <j.a.villas@live.com.ph>
2024-11-22 10:53:04 +0100

    updated signup page added an alternative page for none authorize access

--- src/pages/Authentication/SignUp.jsx
+++ src/pages/Authentication/SignUp.jsx
@@ -1,13 +1,15 @@
-import React, {useState} from 'react';
+import React, {useContext, useState} from 'react';
 import {Link} from "react-router-dom";
 import "./Profile.css"
 import mapRolesToArray from "../../helpers/mapRolesToArray.js";
 import axios from "axios";
+import {AuthContext} from "../../context/AuthContext.jsx";
 
 function SignUp() {
 
     const [errors, setErrors] = useState(null);
     const [successMessage, setSuccessMessage] = useState(null);
+    const {isAuth} = useContext(AuthContext);
 
     const [formUser, setFormUser] = useState({
         userName: "",
@@ -62,8 +64,8 @@ function SignUp() {
 
 
     const handleRoleChange = (e) => {
-        const { name, checked } = e.target;
-        setRoles({ ...roles, [name]: checked });
+        const {name, checked} = e.target;
+        setRoles({...roles, [name]: checked});
     };
 
     const validateForm = () => {
@@ -80,93 +82,111 @@ function SignUp() {
 
     return (
         <section className={"container"}>
-            <article className={"container signup-container"}>
-                <h2>Registreren</h2>
+            {isAuth ? (
+                <article className={"container signup-container"}>
+                    <h2>Registreren</h2>
 
-                {successMessage && <p className="success-message">{successMessage}</p>}
-                <form onSubmit={handleSubmit}>
-                    <div className={"form-group"}>
-                        <label htmlFor="username"> Username</label>
-                        <input
-                            type="text"
-                            id={"username"}
-                            name={"userName"}
-                            value={formUser.userName}
-                            onChange={handleChange}
-                            className={errors?.userName ? "input-error" : ""}
-                        />
-                        {errors?.userName && <p className={"error-message"}>{errors.userName}</p>}
-                    </div>
-
-                    <div className={"form-group"}>
-                        <label htmlFor="password"> Password</label>
-                        <input
-                            type="text"
-                            id={"password"}
-                            name={"password"}
-                            value={formUser.password}
-                            onChange={handleChange}
-                            className={errors?.password ? "input-error" : ""}
-                        />
-                        {errors?.password && <p className={"error-message"}>{errors.password}</p>}
-                    </div>
-
-                    <div className={"form-group"}>
-                        <label htmlFor="confirmPassword">Confirm Password</label>
-                        <input
-                            type="text"
-                            id={"confirmPassword"}
-                            name={"confirmPassword"}
-                            value={formUser.confirmPassword}
-                            onChange={handleChange}
-                            className={errors?.confirmPassword ? "input-error" : ""}
-                        />
-                        {errors?.confirmPassword && <p className={"error-message"}>{errors.confirmPassword}</p>}
-                    </div>
-
-
-                    <div className={"roles"}>
-                        <h4>Select Roles</h4>
-                        <div className="role-checkbox">
-                            <label>
-                                <input
-                                    type="checkbox"
-                                    name="admin"
-                                    checked={roles.admin}
-                                    onChange={handleRoleChange}
-                                />
-                                Admin
-                            </label>
+                    {successMessage && <p className="success-message">{successMessage}</p>}
+                    <form onSubmit={handleSubmit}>
+                        <div className={"form-group"}>
+                            <label htmlFor="username"> Username</label>
+                            <input
+                                type="text"
+                                id={"username"}
+                                name={"userName"}
+                                value={formUser.userName}
+                                onChange={handleChange}
+                                className={errors?.userName ? "input-error" : ""}
+                            />
+                            {errors?.userName && <p className={"error-message"}>{errors.userName}</p>}
                         </div>
-                        <div className="role-checkbox">
-                            <label>
-                                <input
-                                    type="checkbox"
-                                    name="user"
-                                    checked={roles.user}
-                                />
-                                User
-                            </label>
+
+                        <div className={"form-group"}>
+                            <label htmlFor="password"> Password</label>
+                            <input
+                                type="text"
+                                id={"password"}
+                                name={"password"}
+                                value={formUser.password}
+                                onChange={handleChange}
+                                className={errors?.password ? "input-error" : ""}
+                            />
+                            {errors?.password && <p className={"error-message"}>{errors.password}</p>}
                         </div>
-                        <div className="role-checkbox">
-                            <label>
-                                <input
-                                    type="checkbox"
-                                    name="libraryStaff"
-                                    checked={roles.libraryStaff}
-                                    onChange={handleRoleChange}
-                                />
-                                Library Staff
-                            </label>
+
+                        <div className={"form-group"}>
+                            <label htmlFor="confirmPassword">Confirm Password</label>
+                            <input
+                                type="text"
+                                id={"confirmPassword"}
+                                name={"confirmPassword"}
+                                value={formUser.confirmPassword}
+                                onChange={handleChange}
+                                className={errors?.confirmPassword ? "input-error" : ""}
+                            />
+                            {errors?.confirmPassword && <p className={"error-message"}>{errors.confirmPassword}</p>}
+                        </div>
+
+
+                        <div className={"roles"}>
+                            <h4>Select Roles</h4>
+                            <div className="role-checkbox">
+                                <label>
+                                    <input
+                                        type="checkbox"
+                                        name="admin"
+                                        checked={roles.admin}
+                                        onChange={handleRoleChange}
+                                    />
+                                    Admin
+                                </label>
+                            </div>
+                            <div className="role-checkbox">
+                                <label>
+                                    <input
+                                        type="checkbox"
+                                        name="user"
+                                        checked={roles.user}
+                                    />
+                                    User
+                                </label>
+                            </div>
+                            <div className="role-checkbox">
+                                <label>
+                                    <input
+                                        type="checkbox"
+                                        name="libraryStaff"
+                                        checked={roles.libraryStaff}
+                                        onChange={handleRoleChange}
+                                    />
+                                    Library Staff
+                                </label>
+                            </div>
+                        </div>
+
+                        <button type={"submit"} className={"submit-button"}>
+                            Signup
+                        </button>
+                    </form>
+                    <p>Heb je al een account? Je kunt je <Link to="/signin">hier</Link> inloggen.</p>
+                </article>
+            ) : (
+                <article className="container contact-library-container">
+                    <div className="contact-library-card">
+                        <h1 className="contact-title">Register Your Membership</h1>
+                        <p className="contact-message">
+                            To register as a member, please visit your nearest library point.
+                        </p>
+                        <p className="contact-details">
+                            Our friendly staff will assist you with the registration process and provide all the
+                            information you need.
+                        </p>
+                        <div className="contact-footer">
+                            <p>For more information, contact us at: <strong>support@librarysystem.com</strong></p>
                         </div>
                     </div>
-
-                    <button type={"submit"} className={"submit-button"}>
-                        Signup
-                    </button>
-                </form>
-                <p>Heb je al een account? Je kunt je <Link to="/signin">hier</Link> inloggen.</p>
-            </article>
+                </article>
+            )}
         </section>
     );
 }
