03a1d2245f4f194fac28431b6379412771599a7a
smallproject <j.a.villas@live.com.ph>
2024-11-22 13:43:46 +0100

    update author create page added authentication

--- src/pages/Author/CardCreateView.jsx
+++ src/pages/Author/CardCreateView.jsx
@@ -1,6 +1,17 @@
 import "./Card.css"
+import {useContext} from "react";
+import {AuthContext} from "../../context/AuthContext.jsx";
+import {useNavigate} from "react-router-dom";
 
 function CardCreateView() {
+    const {isAuth} = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    if (!isAuth) {
+        navigate("/signin");
+        return null;
+    }
+
     return (
         <section>
             <article>
