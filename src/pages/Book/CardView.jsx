0194893beb077a5be0cc5f3286ecdd6b366083cb
smallproject <j.a.villas@live.com.ph>
2024-11-29 15:39:17 +0100

    updated: replaced go back link with navigate(-1) = go back 1 page

--- src/pages/Book/CardView.jsx
+++ src/pages/Book/CardView.jsx
@@ -91,6 +91,10 @@ function CardView() {
         }
     }
 
+    function handleGoBack() {
+        navigate(-1);
+    }
+
     return (
         <>
             <article className={"card"}>
@@ -99,6 +103,9 @@ function CardView() {
                 {book ? (
                     <ul className={"data-info-list"}>
                         <li className={"data-info-item"}><span className={"link-return-overview"}><Link to={"/api/v1/books"}>Go back</Link></span></li>
+                        <li className={"data-info-item"}><span className={"link-return-overview"}><a href={"#!"}
+                                                                                                     onClick={handleGoBack}>Go back</a></span>
+                        </li>
 
                         {(roles.includes("ROLE_ADMIN") || roles.includes("ROLE_LIBRARY_STAFF")) && (
                             <span className={"buttons"}>
