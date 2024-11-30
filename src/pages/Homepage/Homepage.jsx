7d1f7da48f7c6c26173b57525bdac95a75801a35
smallproject <j.a.villas@live.com.ph>
2024-11-30 07:53:54 +0100

    added Homepage

---
+++ src/pages/Homepage/Homepage.jsx
@@ -0,0 +1,23 @@
+import "./Homepage.css";
+
+function Homepage() {
+
+    return (
+        <section className={"container"}>
+            <article className="container home-container">
+                <h1>Welcome to the Library System</h1>
+                <p>Explore, borrow, and manage books easily with our online platform.</p>
+                <div className="home-actions">
+                    <button className="home-button" onClick={() => (window.location.href = "/about")}>
+                        Learn More
+                    </button>
+                    <button className="home-button" onClick={() => (window.location.href = "/contact")}>
+                        Contact Us
+                    </button>
+                </div>
+            </article>
+        </section>
+    );
+}
+
+export default Homepage;
