cf234b87a7d7772231488047c381290bc4fd722d
smallproject <j.a.villas@live.com.ph>
2025-10-01 11:07:25 +0200

    redesign to implement bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+// import './index.css'
 import {BrowserRouter as Router} from "react-router-dom";
 import LanguageContextProvider from "./context/LanguageContext.jsx";
 import ThemeContextProvider from "./context/ThemeContext.jsx";
 import AuthContextProvider from "./context/AuthContext.jsx";
 import ReservationProvider from "./context/ReservationProvider.jsx";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 
 createRoot(document.getElementById('root')).render(
